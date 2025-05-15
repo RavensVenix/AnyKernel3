d2afcadc9df4e3a1eb6f6f101c043908876a48d9
Caeszx. <ravensvenix@gmail.com>
2025-05-16 05:47:25 +0800

    AnyKernel3: Init Elaina - Celestria

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Elaina - Milestria | POCO X3 NFC
+kernel.string=Elaina - Celestria | POCO X3 NFC
 do.devicecheck=1
 do.modules=0
 do.systemless=1
